3f4a60cd98dd79ec3ab24fbfd29f05b8dfcff551
xdebbie <deh@live.in>
2021-02-04 12:25:14 +0100

    Added input clearing

--- src/containers/NewTodo.js
+++ src/containers/NewTodo.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
-import { View, Text, TextInput, Button } from 'react-native'
+import { View, TextInput, Button } from 'react-native'
 
 export class AddTodo extends Component {
     constructor() {
         super()
         this.state = {
             addTodo: '',
+            empty: '',
         }
     }
 
@@ -18,6 +19,11 @@ export class AddTodo extends Component {
                     style={{ height: 40, marginTop: 40 }}
                     placeholder="Write your chores..."
                     onChangeText={addTodo => this.setState({ addTodo })}
+                    clearButtonMode="always"
+                    // to clear the input field after submitted
+                    ref={input => {
+                        this.TextInput = input
+                    }}
                 />
                 <Button
                     color="#ff0056"
@@ -26,6 +32,8 @@ export class AddTodo extends Component {
                             return
                         } else {
                             this.props.dispatch(addTodo(this.state.addTodo))
+                            // clearing the input
+                            this.TextInput.clear()
                             console.log(this.props)
                         }
                     }}
